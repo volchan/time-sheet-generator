7f9a44402aaba4ac18a5cc425b607c0da588a1ef
stephane de la torre <s.dlt@hotmail.fr>
2018-06-13 15:37:53 +0200

    xls ans csv import ok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,28 +5,50 @@ module ApplicationHelper
 
   def weekend_array(date)
     [
-      date,
-      'Week-end',
-      'Week-end',
-      'Week-end',
-      'Week-end',
-      'Week-end',
-      'Week-end',
-      'Week-end',
-      'Week-end',
-      'Week-end'
+      [
+        date,
+        'Week-end',
+        'Week-end',
+        'Week-end',
+        'Week-end',
+        'Week-end',
+        'Week-end',
+        'Week-end',
+        'Week-end',
+        'Week-end'
+      ], %I[
+        date
+        string
+        string
+        string
+        string
+        string
+        string
+        string
+        string
+        string
+      ]
     ]
   end
 
   def week_array(date, sheet)
     index = sheet.rows.last.row_index + 2
     [
-      date,
-      '00:00:00',
-      '00:00:00',
-      '00:00:00',
-      '00:00:00',
-      "=IF(A#{index}<>\"\",E#{index}-B#{index}-D#{index}+C#{index},\"\")"
+      [
+        date,
+        '00:00:00',
+        '00:00:00',
+        '00:00:00',
+        '00:00:00',
+        "=IF(A#{index}<>\"\",E#{index}-B#{index}-D#{index}+C#{index},\"\")"
+      ], %I[
+        date
+        time
+        time
+        time
+        time
+        time
+      ]
     ]
   end
 end
