3f3ffa575b734b248d78bf2dd6b6370e95a1262a
stephane de la torre <s.dlt@hotmail.fr>
2018-07-07 19:20:10 +0200

    total time working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,15 +40,56 @@ module ApplicationHelper
         '00:00:00',
         '00:00:00',
         '00:00:00',
-        "=TEXT((E#{index}-B#{index}-D#{index}+C#{index}),\"HH:MM:SS\")"
+        "=TEXT((E#{index}-B#{index}-D#{index}+C#{index}),\"HH:MM:SS\")",
+        '07:30:00',
+        '00:00:00',
+        '00:00:00',
+        "=F#{index}-(H#{index}+I#{index})"
       ], %I[
         date
         time
         time
         time
         time
+        string
         time
+        time
+        time
+        string
       ]
     ]
+  end
+
+  def overtime_row(dates, date_index, sheet)
+    week_days = count_weekdays(dates, date_index - 1)
+    cell_array = build_cell_array(week_days, sheet)
+    [
+      "Total H Supp semain #{dates[date_index - 1].cweek}",
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      "=SUM(#{cell_array.join(',')})*24-35",
+      "=IF(J16 > 8, (((J16 - 8)  * 1,5) +  (8  * 1,25)), IF( J16 >= 0,  (J16  * 1,25), J16 ))"
+    ]
+  end
+
+  def count_weekdays(dates, index)
+    dates.select do |date|
+      date.cweek == dates[index].cweek
+    end.length
+  end
+
+  def build_cell_array(week_days, sheet)
+    index = sheet.rows.last.row_index
+    array = []
+    week_days.times do |i|
+      array << "J#{index - i + 1}"
+    end
+    array.reverse
   end
 end
