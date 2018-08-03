778790ebcae35bbc79a4649e4caf7e32d89a7bd0
stephane de la torre <s.dlt@hotmail.fr>
2018-08-03 11:29:00 +0200

    data ok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,70 +1,49 @@
 module ApplicationHelper
   def build_row_array(date, sheet)
-    date.saturday? || date.sunday? ? weekend_array(date) : week_array(date, sheet)
+    date.saturday? || date.sunday? ? weekend_array(date, sheet) : week_array(date, sheet)
   end
 
-  def weekend_array(date)
-    [
-      [
-        date,
-        'Week-end',
-        'Week-end',
-        'Week-end',
-        'Week-end',
-        'Week-end',
-        'Week-end',
-        'Week-end',
-        'Week-end',
-        'Week-end'
-      ], %I[
-        date
-        string
-        string
-        string
-        string
-        string
-        string
-        string
-        string
-        string
-      ]
+  def weekend_array(date, sheet)
+    cells = [
+      date,
+      'Week-end',
+      'Week-end',
+      'Week-end',
+      'Week-end',
+      'Week-end',
+      'Week-end',
+      'Week-end',
+      'Week-end',
+      'Week-end'
     ]
+    sheet.add_row(cells)
   end
 
   def week_array(date, sheet)
     index = sheet.rows.last.row_index + 2
-    [
-      [
-        date,
-        '00:00:00',
-        '00:00:00',
-        '00:00:00',
-        '00:00:00',
-        "=TEXT((E#{index}-B#{index}-D#{index}+C#{index}),\"HH:MM:SS\")",
-        '07:30:00',
-        '00:00:00',
-        '00:00:00',
-        "=F#{index}-(H#{index}+I#{index})"
-      ], %I[
-        date
-        time
-        time
-        time
-        time
-        string
-        time
-        time
-        time
-        string
-      ]
+    cells = [
+      date.strftime('%d/%m/%Y'),
+      '00:00',
+      '00:00',
+      '00:00',
+      '00:00',
+      "=IF(A#{index}<>\"\",E#{index}-B#{index}-D#{index}+C#{index},\"\")",
+      '07:30',
+      '00:00',
+      '00:00',
+      "=F#{index}-(H#{index}+I#{index})"
     ]
+    hrs = sheet.styles.add_style(format_code: 'h:mm')
+    sheet.add_row(cells, style: hrs)
   end
 
   def overtime_row(dates, date_index, sheet)
-    week_days = count_weekdays(dates, date_index - 1)
-    cell_array = build_cell_array(week_days, sheet)
-    [
-      "Total H Supp semain #{dates[date_index - 1].cweek}",
+    days = count_days(dates, date_index - 1)
+    cell_array = build_cell_array(days, sheet)
+    weekdays = count_weekdays(dates, date_index - 1)
+    index = sheet.rows.last.row_index + 2
+    cells = [
+      "Total H Supp semaine #{dates[date_index - 1].cweek}",
       '',
       '',
       '',
@@ -73,13 +52,21 @@ module ApplicationHelper
       '',
       '',
       '',
-      "=SUM(#{cell_array.join(',')})*24-35",
-      "=IF(J16 > 8, (((J16 - 8)  * 1,5) +  (8  * 1,25)), IF( J16 >= 0,  (J16  * 1,25), J16 ))"
+      "=SUM(#{cell_array.join(',')})*24-#{weekdays * 7}",
+      "=IF(J#{index} > 8, (((J#{index} - 8)  * 1.5) + (8  * 1.25)), IF( J#{index} >= 0, (J#{index}  * 1.25), J#{index}))"
     ]
+    sheet.add_row(cells)
+  end
+
+  def count_days(dates, index)
+    dates.select do |date|
+      date.cweek == dates[index].cweek
+    end.length
   end
 
   def count_weekdays(dates, index)
     dates.select do |date|
+      next if date.saturday? || date.sunday?
       date.cweek == dates[index].cweek
     end.length
   end
