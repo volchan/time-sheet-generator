0916aea1ba91998e7052eb95a688f61cbafcadf4
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2019-10-30 20:21:07 +0100

    Fix env var

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'time_sheets#home'
-  default_url_options(host: ENV['HOST'] || 'localhost:3000')
+  default_url_options(host: ENV['url'] || 'localhost:3000')
   resources :time_sheets, only: :create do
     collection do
       get 'download/:folder', action: :download, as: :download
